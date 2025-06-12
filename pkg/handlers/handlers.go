eb6696aaaeb87baa015765b04ba228c32da6e8ff
Ian Park <12iandpark@gmail.com>
2025-06-11 21:23:40 -0500

    Updated Go templates with HTML pages. Updated routes.

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -31,21 +31,35 @@ func NewHandlers(r *Repository) {
 
 // Home is the home page handler
 func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
-
-	remoteIP := r.RemoteAddr
-	m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
-
 	render.RenderTemplate(w, "home.page.tmpl", &models.TemplateData{})
 }
 
 // About is the about page handler
 func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
-	stringMap := make(map[string]string)
-	stringMap["test"] = "Hello, again."
-	stringMap["Your_IP"] = m.App.Session.GetString(r.Context(), "remote_ip")
+	render.RenderTemplate(w, "about.page.tmpl", &models.TemplateData{})
 
-	render.RenderTemplate(w, "about.page.tmpl", &models.TemplateData{
-		StringMap: stringMap,
-	})
+}
+
+// Couple is the about couples-suite handler
+func (m *Repository) Couples(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "couples.page.tmpl", &models.TemplateData{})
+
+}
+
+// Mountain is the mountain-view page handler
+func (m *Repository) Mountain(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "mountain.page.tmpl", &models.TemplateData{})
+
+}
+
+// Book is the booking page handler
+func (m *Repository) Book(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "book.page.tmpl", &models.TemplateData{})
+
+}
+
+// Contact is the contact page handler
+func (m *Repository) Contact(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "contact.page.tmpl", &models.TemplateData{})
 
 }
